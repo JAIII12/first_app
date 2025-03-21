981a6287696a3ddbe5dcde5eeda1af675a0b041a
JAI <sy9534411@gmail.com>
2025-03-22 01:57:41 +0530

    adding exercises

---
+++ lib/util/Exercise_.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Exercise extends StatelessWidget {
+  final IconData icon;
+  final String exerciseName;
+  final int numberOfExercises;
+  final Color color;
+
+  const Exercise({
+    super.key,
+    required this.icon,
+    required this.exerciseName,
+    required this.numberOfExercises,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    color: color, // Ensure a valid color is passed
+                    child: Icon(
+                      icon, // Ensure a valid icon is passed
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Title
+                    Text(
+                      exerciseName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: color,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    // Subtitle
+                    Text(
+                      "$numberOfExercises Exercises",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Icon(Icons.more_horiz),
+          ],
+        ),
+      ),
+    );
+  }
+}
